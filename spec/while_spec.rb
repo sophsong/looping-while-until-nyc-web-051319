ef6c65a7de41947b99924d8741f6f32236afd14b
Sophia Song <ososanne@gmail.com>
2019-05-07 21:32:41 +0000

    Automatically backed up by Learn

--- spec/while_spec.rb
+++ spec/while_spec.rb
@@ -1,16 +1,6 @@
 require_relative "spec_helper"
 require_relative "../lib/while.rb"
 
-def using_while
-levitation_force=  6
-	while levitation_force < 10
-		
-	levitation_force += 1  
-		puts "Wingardium Leviosa #{levitation_force}"
-	
-	end
-end
-
 
 describe "#using_while" do 
 	it "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10" do 
