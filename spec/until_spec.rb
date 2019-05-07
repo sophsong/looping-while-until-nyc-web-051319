754a787a73abaefdec3bdfd254e7cf0a0a7ad72f
Sophia Song <ososanne@gmail.com>
2019-05-07 21:50:51 +0000

    Automatically backed up by Learn

--- spec/until_spec.rb
+++ spec/until_spec.rb
@@ -5,7 +5,7 @@ require_relative "../lib/until.rb"
 
 describe "#using_until" do
 	it "puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
-	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	  looping_string = "Wingardium Leviosa 7\nWingardium Leviosa 8\nWingardium Leviosa 9\nWingardium Leviosa 10\n"
 	  expect{ using_until }.to output(looping_string).to_stdout
   end
 end
